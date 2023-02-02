4eecdd8605e2143e8a24bee29f033244fe31fc41
Romain Trochu <romain.trochu@gmail.com>
2023-02-02 10:42:55 +0100

    add style to dish

--- ecf-react/src/component/displayMenu.jsx
+++ ecf-react/src/component/displayMenu.jsx
@@ -6,16 +6,15 @@ export default function DisplayMenu() {
     return(
 
         <div className=' container flex'>
-
-        
-                {meals.map((item, index) =>
-                    <div className='dish'>
-                        <img src={item.imageSrc} alt="" />
-                        <h2 key={index}>{item.title}</h2>
-                        <span>{item.content}</span>
-                        <span>{item.price}</span>
-                    </div>
-                )}
+            
+            {meals.map((item, index) =>
+                <div className='dish'>
+                    <img src={item.imageSrc} alt="" />
+                    <h2 key={index}>{item.title}</h2>
+                    <span>{item.content}</span>
+                    <span>{item.price}</span>
+                </div>
+            )}
             
         </div>
     )
--- ecf-react/src/index.js
+++ ecf-react/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import DisplayMenu from './component/displayMenu';
+import '../src/App.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
