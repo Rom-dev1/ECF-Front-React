442efcf21941600a16dc1860ee1765ab3459a385
Romain Trochu <romain.trochu@gmail.com>
2023-02-05 11:12:18 +0100

    modify and add name of route

--- ecf-react/src/index.js
+++ ecf-react/src/index.js
@@ -6,6 +6,7 @@ import '../src/App.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import OrderSucess from './component/orderSucess';
 import Meat from './component/meat';
+import Vegetable from './component/vegetables';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,6 +17,7 @@ root.render(
         <Route index element ={<DisplayMenu/>} />
         <Route path="/commande/:title" element={<OrderSucess/>} />
         <Route path="/repas/:viande" element={<Meat/>} />
+        <Route path="/vege/legumes" element={<Vegetable/>} />
       </Routes>
     </BrowserRouter>
   </>
